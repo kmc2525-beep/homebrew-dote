4a22fcec8a200f383d9157c40df27dd659e4d80f
kmc2525-beep <kmc2525@kamiyama.ac.jp>
2025-10-28 13:52:08 +0900

    Update dote.rb

--- dote.rb
+++ dote.rb
@@ -1,7 +1,7 @@
 class Dote < Formula
   desc "dot e command"
   homepage "https://github.com/kmc2525-beep/homebrew-dote"
-  url "https://raw.githubusercontent.com/kmc2400/homebrew-dote/main/dote.c"
+  url "https://raw.githubusercontent.com/kmc2525-beep/homebrew-dote/main/dote.c"
   sha256 "e50d9c0e785abb793f728f559fba106ce5229826344d69640e6e0ee264277a36"
   version "1.0.0"
 
